710b5fed64aa2a59985566469f2e42f2091083d2
parthasarathi m <parthasarathim@parthasarathis-MacBook-Air.local>
2024-03-13 13:32:55 +0530

    hello2.js

---
+++ tasks/hello2.js
@@ -0,0 +1,44 @@
+//SUM OF ALL ARRAYS//
+//1.ANONYMUS FUNCTION//
+/*
+const numbersArray = [1, 2, 3, 4, 5];
+
+// Using arrow function for sum of numbers
+const sum = ((arr) => {
+    return arr.reduce((total, num) => {
+        return total + num;
+    }, 0);
+})(numbersArray);
+
+console.log("Sum:", sum);
+;*/
+
+
+//using arrow fumction//
+
+const numbersArray = [1, 2, 3, 4, 5];
+
+// Using arrow function for sum of numbers
+/*const sum = ((arr) => {
+    return arr.reduce((total, num) => {
+        return total + num;
+    }, 0);
+})(numbersArray);
+
+console.log("Sum:", sum);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
